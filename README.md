75cad890c58ceb7ff10f1a56bff2bbe25526d771
Anatoly Romanov <tromanov95@mail.ru>
2022-07-29 17:26:26 +0300

    deleted cache

---
+++ README.md
@@ -0,0 +1,64 @@
+# Бот для встреч футбольного сообщества 
+## Технические требования
+- MongoDB
+- NodeJS
+## Установка на сервер
+1. Арендовать удаленный сервер Ubuntu.
+2. [Зарегистрировать телеграм-бота и получить токен](https://core.telegram.org/bots#3-how-do-i-create-a-bot)
+3. Зайти на сервер через SSH ```ssh root@{server_ip}```
+4. [Установить NodeJS и npm по инструкции](https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-20-04-ru) или выполнить команды ниже<br>
+```cd ~```<br>
+```curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh```<br>
+```sudo bash nodesource_setup.sh```<br>
+```sudo apt install nodejs```<br>
+```sudo apt install npm```
+4. [Установить MongoDB](https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/#install-mongodb-community-edition) и выполнить следующие команды
+```systemctl enable mongod```
+```systemctl start mongod```
+```sudo reboot```<br>Последняя команда перезагружает сервер, поэтому ждём, подключаемся к серверу заново и идём дальше.
+5. Идём в корень сервера ```cd ~``` и скачиваем репозиторий ```git clone репозитория```
+6. Идем в папку репозитория ```cd fan_club_bot```
+7. В файл token.js вставляем токен бота, полученный на шаге 2.
+8. Устанавливаем npm-пакеты ```npm i```
+9. Запускаем бота ```npm run start```<br>
+10. Если все хорошо, увидишь что-то такое
+![image](https://user-images.githubusercontent.com/18449287/181778194-56a6bf34-7bb2-49be-bc3f-3c3a2c594704.png)
+
+## Схемы таблиц БД
+users (Пользователи)
+```
+{
+  _id: ObjectId;
+  telegramUserId: Int32;
+  telegramUsername: String;
+  name: String;
+  admin: Boolean;
+}
+```
+events (События/встречи)
+```
+{
+  _id: ObjectId;
+  name: String;
+  place: String;
+  datetime: String;
+  active: Boolean;
+}
+```
+records (Записи/регистрации на встречи)
+```
+{
+  _id: ObjectId;
+  eventId: String; // events._id
+  userId: Int32; // users.telegramUserId
+  number: Int32;
+}
+```
+arrivals (Отметки прихода гостей на встречи)
+```
+{
+  _id: ObjectId;
+  eventId: String; // events._id
+  userId: Int32; // users.telegramUserId
+}
+```
---
+++ index.js
@@ -0,0 +1,155 @@
+import TelegramBot from "node-telegram-bot-api";
+import { TELEGRAM_API_TOKEN } from "./token.js";
+import { connectToDatabase, findUser } from "./mongo.js";
+
+import {
+  addEvent,
+  handleDisableEventButton,
+  viewActiveEvents,
+  handleRegisterOnEventButton,
+  handleCancelRegistrationButton,
+  disableEvent,
+  registerOnEvent,
+  cancelRegistration,
+  markArrival,
+  handleMarkArrivalButton,
+  handleViewCountButton,
+  viewCount,
+} from "./events.js";
+import { handleUserStart, getUsers } from "./users.js";
+
+const mainButtons = [
+  {
+    text: "Добавить мероприятие",
+    public: false,
+    callback_data: JSON.stringify({ method: "addEvent" }),
+  },
+  {
+    text: "Удалить мероприятие",
+    public: false,
+    callback_data: JSON.stringify({
+      method: "handleDisableEventButton",
+    }),
+  },
+  {
+    text: "Отметить пришедшего",
+    public: false,
+    callback_data: JSON.stringify({
+      method: "handleMarkArrivalButton",
+    }),
+  },
+  {
+    text: "Предстоящие мероприятия",
+    public: false,
+    callback_data: JSON.stringify({ method: "viewEvents" }),
+  },
+
+  {
+    text: "Записаться на мероприятие",
+    public: true,
+    callback_data: JSON.stringify({
+      method: "handleRegisterOnEventButton",
+    }),
+  },
+  {
+    text: "Отменить запись на мероприятие ",
+    public: true,
+    callback_data: JSON.stringify({
+      method: "handleCancelRegistrationButton",
+    }),
+  },
+  {
+    text: "Показать записавшихся",
+    public: true,
+    callback_data: JSON.stringify({
+      method: "handleViewCountButton",
+    }),
+  },
+];
+
+const bot = new TelegramBot(TELEGRAM_API_TOKEN, { polling: true });
+
+await connectToDatabase();
+
+bot.onText(/\/start/, async (msg) => {
+  await handleUserStart(msg, bot);
+});
+
+bot.onText(/\/users/, async (msg) => {
+  await getUsers(msg, bot);
+});
+
+bot.onText(/\/menu/, async (msg) => {
+  const chatId = msg.chat.id;
+  const userId = msg.from.id;
+
+  const user = await findUser(userId);
+  if (user) {
+    const inlineKeyboard = user.admin
+      ? mainButtons
+      : mainButtons.filter((b) => b.public === true);
+
+    bot.sendMessage(chatId, "Чего хочешь?", {
+      reply_markup: {
+        inline_keyboard: inlineKeyboard.map((x) => [x]),
+      },
+    });
+  } else {
+    bot.sendMessage(
+      chatId,
+      "Вы пока не зарегистрированы в фан-клубе. Нажми /start, чтобы зарегистрироваться."
+    );
+  }
+});
+
+bot.on("callback_query", (callbackQuery) => {
+  const chatId = callbackQuery.message.chat.id;
+  const userId = callbackQuery.from.id;
+
+  const queryData = JSON.parse(callbackQuery.data);
+
+  switch (queryData.method) {
+    case "addEvent":
+      addEvent(bot, chatId);
+      break;
+    case "viewEvents":
+      viewActiveEvents(bot, chatId);
+      break;
+    case "handleDisableEventButton":
+      handleDisableEventButton(bot, chatId);
+      break;
+    case "disableEvent":
+      disableEvent(bot, chatId, queryData);
+      break;
+    case "handleRegisterOnEventButton":
+      handleRegisterOnEventButton(bot, chatId);
+      break;
+    case "handleCancelRegistrationButton":
+      handleCancelRegistrationButton(bot, chatId, userId);
+      break;
+    case "regOnEvent":
+      registerOnEvent(bot, chatId, userId, queryData);
+      break;
+    case "cancelReg":
+      cancelRegistration(bot, chatId, userId, queryData);
+      break;
+    case "handleMarkArrivalButton":
+      handleMarkArrivalButton(bot, chatId);
+      break;
+    case "markArrival":
+      markArrival(bot, chatId, queryData);
+      break;
+    case "handleViewCountButton":
+      handleViewCountButton(bot, chatId);
+      break;
+    case "viewCount":
+      viewCount(bot, chatId, queryData);
+      break;
+    default:
+      bot.sendMessage(chatId, "Непонятно, давай попробуем ещё раз?", {
+        reply_markup: {
+          inline_keyboard: menuKeyboard,
+        },
+      });
+  }
+});
